863a41f7cc7cb3c3662b0141c0f167b69a844b3f
Morgan <morgieglover@gmail.com>
2019-07-20 13:30:30 -0500

    FixTestPrepBackgroud
    -fix backround
    -add to comment section in 'help us'
    -add to js for comments

--- tbJava.js
+++ tbJava.js
@@ -1,5 +1,19 @@
-function take() {
-  var comment = document.getElementById("test").value;
-    return
-  console.log("It is ready" + comment)
+
+function addComment() {
+  var table = document.getElementById("table");
+
+  var row= document.createElement("tr");
+  var td1 = document.createElement("td");
+
+
+  td1.innerHTML = document.getElementById("comment").value;
+
+  row.appendChild(td1);
+
+  table.children[0].appendChild(row);
+
+}
+
+function saveFiletotxt() {
+  
 }
