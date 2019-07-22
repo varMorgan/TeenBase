d2970331fcb5f6df9b4498151fb37bc4cdeebfb5
Morgan <morgieglover@gmail.com>
2019-07-22 09:23:01 -0500

    MAKE IT WORK I WANT TO DIE!!!

--- tbJava.js
+++ tbJava.js
@@ -1,19 +1,28 @@
 
-function addComment() {
+// function addComment() {
+//   var table = document.getElementById("table");
+//
+//   var row= document.createElement("tr");
+//   var td1 = document.createElement("td");
+//
+//
+//   td1.innerHTML = document.getElementById("comment").value;
+//
+//   row.appendChild(td1);
+//
+//   table.children[0].appendChild(row);
+//
+// }
+
+function saveForm() {
   var table = document.getElementById("table");
 
-  var row= document.createElement("tr");
-  var td1 = document.createElement("td");
+  var row = table.insertRow(0);
 
+  var cell1 = row.inserCell(0);
 
-  td1.innerHTML = document.getElementById("comment").value;
-
-  row.appendChild(td1);
-
-  table.children[0].appendChild(row);
+  cell1.innerHTML = "tesr";
 
 }
 
-function saveFiletotxt() {
-  
-}
+//https://www.w3schools.com/jsref/met_table_insertrow.asp
