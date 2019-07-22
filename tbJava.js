60a883803caa649269d482ff27cc727df9192a45
Morgan <morgieglover@gmail.com>
2019-07-22 15:40:31 -0500

    hehehheh

--- tbJava.js
+++ tbJava.js
@@ -25,4 +25,21 @@ function saveForm() {
 
 }
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topbtn").style.display = "block";
+  } else {
+    document.getElementById("topbtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 //https://www.w3schools.com/jsref/met_table_insertrow.asp
