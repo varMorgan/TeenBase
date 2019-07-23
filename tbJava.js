f2117878c6319abed948d04ec273b77bbc172bd4
Calla <53015711+julieann4536@users.noreply.github.com>
2019-07-23 10:45:50 -0500

    gih

--- tbJava.js
+++ tbJava.js
@@ -24,8 +24,9 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
 
-  
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -47,33 +48,3 @@ function topFunction() {
 //   table.children[0].appendChild(row);
 //
 // }
-
-function saveForm() {
-  var table = document.getElementById("table");
-
-  var row = table.insertRow(0);
-
-  var cell1 = row.inserCell(0);
-
-  cell1.innerHTML = "tesr";
-
-}
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("topbtn").style.display = "block";
-  } else {
-    document.getElementById("topbtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-//https://www.w3schools.com/jsref/met_table_insertrow.asp
